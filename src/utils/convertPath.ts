393499b9f23bbc0c388ac4ac440783430a5efd8b
Daniel Costrasel <ohkimur@gmail.com>
2021-09-02 13:07:11 +0300

    refactor: improved var names

--- src/utils/convertPath.ts
+++ src/utils/convertPath.ts
@@ -1,10 +1,11 @@
 import path from 'path';
 
 const absoluteUrlRegExp = /(href|src)="(?!http[s]|ftp?:\/\/)([^"|#]+)"/g;
+
 export const isDirectory = (dirPath: string) => path.extname(dirPath) === '';
 
-export const convertAbsolutePathsToRelative = (content: string, filePath: string) =>
-  content.replace(absoluteUrlRegExp, (_absoluteUrl: string, $1: string, $2: string) => {
+export const convertAbsolutePathsToRelative = (fileContent: string, filePath: string) =>
+  fileContent.replace(absoluteUrlRegExp, (_absoluteUrl: string, $1: string, $2: string) => {
     const currentDirPath = path.dirname(filePath);
     const relativeDirPath = currentDirPath === '.' ? '.' : path.relative(currentDirPath, '');
 
