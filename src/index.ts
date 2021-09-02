b6285abd4afa7060f831ce2be50c4cf74c22775e
Daniel Costrasel <ohkimur@gmail.com>
2021-09-02 13:18:33 +0300

    refactor: improved type checking

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { LoadContext, Props } from '@docusaurus/types';
 import { convertAbsolutePathsToRelative } from './utils/convertPath';
 
 const websiteTextualFileExtensions = ['.css', '.js', '.html', '.xml'];
-const isNotWebsiteTextualFile = (filePath: string, stats: any) =>
+const isNotWebsiteTextualFile = (filePath: string, stats: fs.Stats) =>
   !(stats.isDirectory() || websiteTextualFileExtensions.includes(path.extname(filePath)));
 
 export default (_context: LoadContext, _options: any) => {
